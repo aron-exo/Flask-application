417ed544885ef67a3218fbf90302f82ced30538d
aron-exo <166415369+aron-exo@users.noreply.github.com>
2024-06-17 17:18:11 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -87,6 +87,7 @@ def query_geometries_within_polygon_for_table(table_name, polygon_geojson):
             )
         );
         """
+        st.write(f"Executing query for table {table_name}: {query}")  # Debug print
         df = pd.read_sql(query, conn)
         conn.close()
 
@@ -127,7 +128,8 @@ def add_geometries_to_map(geojson_list, metadata_list, map_object):
 
         srid = metadata.pop('srid')
         table_name = metadata.pop('table_name')
-        drawing_info = json.loads(metadata.pop('drawing_info', '{}'))
+        drawing_info_str = metadata.pop('drawing_info', '{}')
+        drawing_info = json.loads(drawing_info_str)
         geometry = json.loads(geojson)
 
         # Define the source and destination coordinate systems
@@ -217,4 +219,3 @@ if st_data and 'last_active_drawing' in st_data and st_data['last_active_drawing
 
 # Display the map using Streamlit-Folium
 st_folium(st.session_state.map, width=700, height=500, key="map")
-
