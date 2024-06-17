5b2daaf87906d19f6fff5137c991cc7b2e229ba9
aron-exo <166415369+aron-exo@users.noreply.github.com>
2024-06-17 17:07:03 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -217,3 +217,4 @@ if st_data and 'last_active_drawing' in st_data and st_data['last_active_drawing
 
 # Display the map using Streamlit-Folium
 st_folium(st.session_state.map, width=700, height=500, key="map")
+
