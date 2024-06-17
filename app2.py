a99a00341b7d9f7da3f4ccecfb63641df4eccbdc
aron-exo <166415369+aron-exo@users.noreply.github.com>
2024-06-17 11:19:14 +0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -22,8 +22,14 @@ if 'table_columns' not in st.session_state:
 # Database connection function for CockroachDB using SQLAlchemy
 def get_engine():
     try:
-        engine = create_engine(st.secrets["DATABASE_URL"])
-        return engine
+        conn = psycopg2.connect(
+            host=st.secrets["db_host"],
+            database=st.secrets["db_name"],
+            user=st.secrets["db_user"],
+            password=st.secrets["db_password"],
+            port=st.secrets["db_port"]
+        )
+        return conn
     except Exception as e:
         st.error(f"Connection error: {e}")
         return None
