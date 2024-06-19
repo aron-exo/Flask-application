880d0867683c88a9225a1f899db77650694b74f6
aron-exo <166415369+aron-exo@users.noreply.github.com>
2024-06-19 15:27:03 +0300

    Update app3.py

--- app3.py
+++ app3.py
@@ -203,11 +203,11 @@ st_data = st_folium(st.session_state.map, width=700, height=500, key="initial_ma
 
 if st_data and 'last_active_drawing' in st_data and st_data['last_active_drawing']:
     polygon_geojson = json.dumps(st_data['last_active_drawing']['geometry'])
-    print(polygon_geojson)
+    st.write(polygon_geojson)
     if st.button('Query Database'):
         try:
             df = query_geometries_within_polygon(polygon_geojson)
-            print(df)
+            st.write(df)
             if not df.empty:
                 st.session_state.geojson_list = df['geometry'].tolist()
                 st.session_state.metadata_list = df.to_dict(orient='records')
